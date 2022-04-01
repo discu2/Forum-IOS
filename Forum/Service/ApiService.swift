4d7a7744eadcd7d1fc7c8ab4587b3363a4408fec
frank89722 <frank89722@gmail.com>
2022-04-01 21:32:43 +0800

    cleanup `ApiService`☺️☺️
    ☺️

--- Forum/Service/ApiService.swift
+++ Forum/Service/ApiService.swift
@@ -16,6 +16,29 @@ class ApiService: DataFetchable {
         self.urlString = urlString
     }
     
+    private func sessionHandler<C: Decodable>(data: Data?, response: URLResponse?, error: Error?, responsePackageType: C.Type, promise: Future<C?, Error>.Promise) {
+        
+        let code = (response as? HTTPURLResponse)?.statusCode ?? 0
+        
+        if code != 200 {
+            promise(.failure(NSError(domain: "", code: code)))
+            return
+        }
+            
+        if let data = data {
+            do {
+                let decoded = try JSONDecoder().decode(responsePackageType, from: data)
+                promise(.success(decoded))
+            } catch {
+                promise(.failure(error))
+            }
+            
+            return
+        }
+        
+        promise(.success(nil))
+    }
+    
     func fetchApi<T: Encodable, C: Decodable>(_ endPointString: String, method: String ,requestPackage: T, responsePackageType: C.Type) -> Future<C?, Error> {
         
         let url = URL(string: urlString + endPointString)!
@@ -26,27 +49,11 @@ class ApiService: DataFetchable {
         let data = try? JSONEncoder().encode(requestPackage)
         request.httpBody = data
         
-        return Future { promise in
+        return Future { [weak self] promise in
             
             URLSession.shared.dataTask(with: request) { data, response, error in
-                
-                let code = (response as? HTTPURLResponse)?.statusCode ?? 0
-                
-                if code == 200 {
-                    
-                    if let data = data {
-                        do {
-                            let decoded = try JSONDecoder().decode(responsePackageType, from: data)
-                            
-                            promise(.success(decoded))
-                        } catch {
-                            promise(.failure(error))
-                        }
-                    }
-                } else {
-                    promise(.failure(NSError(domain: endPointString, code: code)))
-                }
-                
+                guard let self = self else { return }
+                self.sessionHandler(data: data, response: response, error: error, responsePackageType: responsePackageType, promise: promise)
             }.resume()
             
         }
@@ -57,26 +64,11 @@ class ApiService: DataFetchable {
         
         let url = URL(string: urlString + endPointString)!
         
-        return Future { promise in
+        return Future { [weak self] promise in
             
             URLSession.shared.dataTask(with: url) { data, response, error in
-                
-                let code = (response as? HTTPURLResponse)?.statusCode ?? 0
-                
-                if code == 200 {
-                    
-                    if let data = data {
-                        do {
-                            let decoded = try JSONDecoder().decode(responsePackageType, from: data)
-                            promise(.success(decoded))
-                        } catch {
-                            promise(.failure(error))
-                        }
-                    }
-                    
-                } else {
-                    promise(.failure(NSError(domain: endPointString, code: code)))
-                }
+                guard let self = self else { return }
+                self.sessionHandler(data: data, response: response, error: error, responsePackageType: responsePackageType, promise: promise)
             }.resume()
             
         }
